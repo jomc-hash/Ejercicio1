69087bb9e4cfc0c9500fb7166a7dd8d6b9df4d6e
jomc-hash <carrizosa.josechema@gmail.com>
2021-09-14 20:02:27 -0500

    Se agregaron propiedades de zona Se agregaron propiedades de persona Se agregó la función que calcula el descuento en la clase propiedades

--- src/Persona.kt
+++ src/Persona.kt
@@ -1,11 +1,27 @@
-class Persona (nombre:String) {
+import java.time.LocalDate
+import java.time.Period
+import java.time.format.DateTimeFormatter
 
+class Persona (nombre:String, var fechaNacimiento:String, var madreSoltera:Boolean=false) {
+    fun edad(): Int {
+        val fechaActual = LocalDate.now()
+        var fecha = LocalDate.parse(fechaNacimiento, DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+        var edadActual= Period.between(
+            fecha,
+            fechaActual
+        ).years
+        return edadActual
+    }
 }
 interface IZona{
+    var clave:String
+    var descripcion:String
     fun calcularCosto(extension: Double) :Double
 }
 
 class zonaMarginada():IZona{
+    override var clave: String= "MAR"
+    override var descripcion ="Marginada"
     override fun calcularCosto(extension:Double): Double {
         return extension*2.00
     }
@@ -13,6 +29,24 @@ class zonaMarginada():IZona{
 
 
 class zonaRural():IZona{
+    override var clave: String= "RUR"
+    override var descripcion ="Rural"
+    override fun calcularCosto(extension:Double): Double {
+        return extension*8.00
+    }
+}
+
+class zonaUrbana():IZona{
+    override var clave: String= "URB"
+    override var descripcion ="Urbana"
+    override fun calcularCosto(extension:Double): Double {
+        return extension*8.00
+    }
+}
+
+class zonaResidencial():IZona{
+    override var clave: String= "RES"
+    override var descripcion ="Residencial"
     override fun calcularCosto(extension:Double): Double {
         return extension*8.00
     }
@@ -26,9 +60,7 @@ class  Predio( zona: IZona, extension: Double){
     fun calcularCosto(): Double {
     return        zona.calcularCosto(extension)
     }
-    fun calcularDescuento(){
 
-    }
 }
 
 
@@ -51,13 +83,31 @@ class Propiedades(persona: Persona){
     }
 
     fun calcularPago(){
-        calcularDescuento()
-        calcularTotal()
+        fun calcularDescuento(): Double {
+            var mes=LocalDate.now().month.value
+            var porcentajeDescuento=0.0
+            if (mes<=2 ){
+                if((persona.edad()>=70 || persona.madreSoltera)){
+                    porcentajeDescuento= 0.7
+                }
+                else {
+                    porcentajeDescuento= 0.4
+                }
+            }
+            else if (persona.edad()>=70 || persona.madreSoltera){
+                porcentajeDescuento= 0.5
+            }
+            var  totalSinDescuento= calcularTotal()
+            return totalSinDescuento - (totalSinDescuento * porcentajeDescuento)
+        }
+
     }
 }
 
 fun main(){
-    var propietario = Persona("Jose Manuel")
+    println(LocalDate.now().month.value)
+
+    var propietario = Persona("Jose Manuel", "12-02-1998")
    var propiedadesJose = Propiedades(propietario)
     propiedadesJose.agregarPropiedad(Predio(zonaRural(),400.0))
     propiedadesJose.agregarPropiedad(Predio(zonaRural(),100.0))
